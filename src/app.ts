835fa2110e8c27285b04941cd0c83c1ade6a79a6
Shahzaib Ansari <61605534+shahzaibansari123@users.noreply.github.com>
2023-02-16 21:33:47 +0500

    Practicing generics

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ data.split(" ");
 
 // helping function
 
-function merge <T, U>(objA : T, objB : U){
+function merge <T extends object, U extends object>(objA : T, objB : U){
     return {...objA, ...objB}
 }
 
