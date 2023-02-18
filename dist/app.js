29cc91e62e7987468f2f9d633c79f3ac4050ae04
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-02-18 22:51:02 +0500

    errors fixed

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,23 @@
 "use strict";
 // const element= <HTMLInputElement>document.getElementById('num1')!
 // element.value
-//GENERICS
-// use to pass or assigned additional info in typescript
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("done");
-    }, 2000);
-});
-promise.then((data) => {
-    data.split(" ");
-});
-// helping function
-function merge(objA, objB) {
-    return Object.assign(Object.assign({}, objA), objB);
-}
-const merged = merge({ name: " Shahzaib" }, { age: 22 });
-console.log(merged.age);
-// DECORATORS
+// ----------GENERICS
+// --------- use to pass or assigned additional info in typescript
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("done");
+//   }, 2000);
+// });
+// promise.then((data) => {
+//   data.split(" ");
+// });
+// ----------helping function
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//   return { ...objA, ...objB };
+// }
+// const merged = merge({ name: " Shahzaib" }, { age: 22 });
+// console.log(merged.age);
+// ------------DECORATORS
 function Logger() { }
 class Person {
     constructor() {
--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,29 @@
 
 // element.value
 
-//GENERICS
-// use to pass or assigned additional info in typescript
+// ----------GENERICS
+// --------- use to pass or assigned additional info in typescript
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("done");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("done");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
 
-// helping function
+// ----------helping function
 
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return { ...objA, ...objB };
-}
+// function merge<T extends object, U extends object>(objA: T, objB: U) {
+//   return { ...objA, ...objB };
+// }
 
-const merged = merge({ name: " Shahzaib" }, { age: 22 });
-console.log(merged.age);
+// const merged = merge({ name: " Shahzaib" }, { age: 22 });
+// console.log(merged.age);
 
-// DECORATORS
+// ------------DECORATORS
 
 function Logger() {}
 
