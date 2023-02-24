2fc4208f0e7d74c2a5e0fe2fd883f680803e32a8
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-02-24 23:37:27 +0500

    Minor Changes

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,11 @@
 "use strict";
 // const element= <HTMLInputElement>document.getElementById('num1')!
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 // element.value
 // ----------GENERICS
 // --------- use to pass or assigned additional info in typescript
@@ -18,13 +24,28 @@
 // const merged = merge({ name: " Shahzaib" }, { age: 22 });
 // console.log(merged.age);
 // ------------DECORATORS
-function Logger() {
-    console.log("Logging....!");
+function Logger(data) {
+    return function (_) {
+        // console.log(constructor)
+        console.log("Logging....=>" + data);
+    };
 }
-class Person {
+function FillHTML(template, id) {
+    return function (_) {
+        const el = document.getElementById(id);
+        if (el) {
+            el.innerHTML = template;
+        }
+    };
+}
+let Person = class Person {
     constructor() {
         this.name = "Shahzaib";
         console.log("called...!");
     }
-}
+};
+Person = __decorate([
+    Logger("data"),
+    FillHTML("<b>Template Data </b>", "app")
+], Person);
 const shahzaib = new Person();
