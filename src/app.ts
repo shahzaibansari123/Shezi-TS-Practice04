fe1a09ca90b8cae3e4b70ecd4b0c677ae9bea428
Shahzaib Ansari <61605534+shahzaibansari123@users.noreply.github.com>
2023-03-02 23:23:23 +0500

    Minor changes

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,8 @@
 // type Combined = string | number;
 
 function add(a: string, b: string): string 
-function add(a: number, b: number) : number {
+function add(a: number, b: number) : number 
+function add(a: Combined, b: Combined){
 if(typeOf a === "number" && typeOf b === "number"){
 return a + b;
 }else if(typeOf a === "string" && typeOf b === "string"){
